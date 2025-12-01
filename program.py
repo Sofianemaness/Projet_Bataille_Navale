4580f4c3a4626361c8dbc97cbad5e9704d9b8610
bilaltaouri <bilaltaouri1@gmail.com>
2025-12-01 11:42:28 +0100

    tiens mec

--- program.py
+++ program.py
@@ -6,20 +6,25 @@ import string
 ##########
 def choix_position():
     position = []
-    size = int(input("Choisissez la taille de votre sous marin, entre 1 et 3 cases: "))
+    size = int(input("Choisissez la taille du sous marin, entre 1 et 3 cases: "))
     orientation = input("Vertical (V) ou horizontal (H): ")
+    profondeur = int(input("Choisissez une profondeur entre 100, 200 et 300 mètres: "))
+    if(profondeur == 100) : profondeur = 0
+    if(profondeur == 200) : profondeur = 1
+    if(profondeur == 300) : profondeur = 2
     x = input('Choisissez la position X du début (de A à E): ')
     y = int(input('Choisissez la position Y du début (de 0 à 9): '))
-    for i in range(size):
-        position.append([x, y])
+    for j in range(size):
         if(orientation == 'V'):
-            if(chr(ord(x) + 1) != 'F') : x = chr(ord(x) + 1) 
+            if(x != 'F') :
+                position.append([x, y])
+                x = chr(ord(x) + 1) 
         else:
-            if(y < 9) : y += 1
+            if(y < 9) : 
+                position.append([x, y])
+                y += 1
+    position.append(profondeur)
     return position
-
-posJoueurA = choix_position()
-posJoueurB = choix_position()
     
 def init_grille():
     grille = []
@@ -34,8 +39,6 @@ def init_grille():
         grille.append(profondeur)
     
     return grille
-
-
 
 def print_grilles(grille):
     lettres = string.ascii_uppercase  # A, B, C...
@@ -61,13 +64,21 @@ def print_grilles(grille):
         print("  +" + "--" * main.TAILLE_GRILLE_HORIZONTAL + "+")
         print("")  # espace entre grilles
 
-     
-     
-
-
 ##########
 #  Main  # 
 ##########
 
+print("JOUEUR A: ")
+nb_sousmarinA = int(input("Combien de sous marins voulez vous? >> "))
+posJoueurA = []
+for i in range(nb_sousmarinA):
+    posJoueurA.append(choix_position())
+print("JOUEUR B: ")
+nb_sousmarinB = int(input("Combien de sous marins voulez vous? >> "))
+posJoueurB = []
+for i in range(nb_sousmarinB):
+    posJoueurB.append(choix_position())
+print("Joueur A: ", posJoueurA)
+print("Joueur B: ", posJoueurB)
 grille = init_grille()
 print_grilles(grille)
