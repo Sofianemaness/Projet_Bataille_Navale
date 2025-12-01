d419f265c8f906bdd186b3875e4da3ffbbef9c11
Sofianemaness <166488963+Sofianemaness@users.noreply.github.com>
2025-12-01 09:20:35 +0000

    implémentation de l'affiche et l'initialisation des grilles incluant les profondeurs

--- program.py
+++ program.py
@@ -1,9 +1,62 @@
+import main
+import string
+
+##########
+#Méthodes# 
+##########
 def choix_position():
     position = []
     while(size > 3 | size < 1):
         size = int(input("Choisissez la taille de votre sous marin, entre 1 et 3 cases: "))
     orientation = input("Vertical (V) ou horizontal (H): ")
     
+def init_grille():
+    grille = []
+
+    for prof in range(main.PROFONDEURS):
+        profondeur = []
+        for i in range(main.TAILLE_GRILLE_VERTICAL):
+            ligne = []
+            for y in range(main.TAILLE_GRILLE_HORIZONTAL):
+                ligne.append(0)
+            profondeur.append(ligne)
+        grille.append(profondeur)
+    
+    return grille
 
 
-choix_position()
+
+def print_grilles(grille):
+    lettres = string.ascii_uppercase  # A, B, C...
+
+    for prof in range(main.PROFONDEURS):
+        print(f"Profondeur {prof}")
+
+        # Affichage des numéros de colonnes
+        print("   ", end="")
+        for col in range(main.TAILLE_GRILLE_HORIZONTAL):
+            print(col, end=" ")
+        print()
+
+        print("  +" + "--" * main.TAILLE_GRILLE_HORIZONTAL + "+")
+
+        # Affichage des lignes
+        for i in range(main.TAILLE_GRILLE_VERTICAL):
+            print(f"{lettres[i]} |", end=" ")
+            for y in range(main.TAILLE_GRILLE_HORIZONTAL):
+                print(grille[prof][i][y], end=" ")
+            print("|")
+
+        print("  +" + "--" * main.TAILLE_GRILLE_HORIZONTAL + "+")
+        print("")  # espace entre grilles
+
+     
+     
+
+
+##########
+#  Main  # 
+##########
+
+grille = init_grille()
+print_grilles(grille)
